d530ea40f7709b6f37242ea8a1363ae0cccebc18
root <saung.myat@gmail.com>
2022-03-06 20:44:30 -0500

    commit tree bf488444556e90179363749b59b6d88a2ed990db

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+ function findMatching(drivers, matchName) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase() === matchName.toLowerCase() 
+    })
+  }
+
+  function fuzzyMatch(drivers, fuzzyName) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().substring(0, fuzzyName.length) === fuzzyName.toLowerCase() 
+    })
+  }
+  
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) {
+        return driver.name === argument 
+    })
+  }
